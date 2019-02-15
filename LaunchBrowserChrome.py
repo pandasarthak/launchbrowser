f49b3d6ffa08a9901e0c2fc724dfe8e4295d4e6d
unknown <you@example.com>
2019-02-15 09:11:04 +0530

    chrome browser launch

---
+++ LaunchBrowserChrome.py
@@ -0,0 +1,3 @@
+from selenium import webdriver
+driver=webdriver.Chrome()
+driver.get("‪file:///C:/Users/sarthak/Desktop/test.html")
---
+++ LaunchbrowserIE.py
@@ -0,0 +1,3 @@
+from selenium import webdriver
+driver=webdriver.Ie("D:/Software/IEDriverServer_Win32_3.14.0/IEDriverServer.exe")
+driver.get("https://www.google.com/")
